fbcf7830586c1ed1cd38e2d27ba0e1651c59f471
heathkd <heather.dsouza@datadoghq.com>
2020-01-16 18:01:55 -0500

    create ScheduledTasks bean and application context config

--- dd-java-agent/instrumentation/spring-scheduling/src/test/groovy/ReportTimeTest.groovy
+++ dd-java-agent/instrumentation/spring-scheduling/src/test/groovy/ReportTimeTest.groovy
@@ -1,6 +1,7 @@
 import datadog.trace.agent.test.AgentTestRunner
 import datadog.trace.agent.test.utils.ConfigUtils
 import datadog.trace.instrumentation.api.Tags
+import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 class ReportTimeTest extends AgentTestRunner {
 
@@ -23,6 +24,31 @@ class ReportTimeTest extends AgentTestRunner {
     setup:
     def scheduledTask = new ScheduledTasks();
     scheduledTask.reportCurrentTime();
+
+    expect:
+    assertTraces(1) {
+      trace(0, 1) {
+        span(0) {
+          serviceName "test"
+          resourceName "ScheduledTasks.reportCurrentTime"
+          operationName "trace.annotation"
+          parent()
+          errored false
+          tags {
+//            "$Tags.COMPONENT" "scheduled"
+            "$Tags.COMPONENT" "trace"
+            defaultTags()
+          }
+        }
+      }
+    }
+  }
+
+  def "test with context"() {
+    setup:
+    def context = new AnnotationConfigApplicationContext(ScheduledTasksConfig) // provide config as argument
+    def task = context.getBean(ScheduledTasks) // provide class we are trying to test
+    task.reportCurrentTime()
 
     expect:
     assertTraces(1) {
