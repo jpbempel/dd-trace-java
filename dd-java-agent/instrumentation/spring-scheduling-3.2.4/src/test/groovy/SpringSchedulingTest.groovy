7b3452389438f6998cceea5e6b907cab2fb304fc
heathkd <heather.dsouza@datadoghq.com>
2020-02-05 19:14:09 -0500

    remove locks and clearing of TEST_WRITER

--- dd-java-agent/instrumentation/spring-scheduling-3.2.4/src/test/groovy/SpringSchedulingTest.groovy
+++ dd-java-agent/instrumentation/spring-scheduling-3.2.4/src/test/groovy/SpringSchedulingTest.groovy
@@ -2,51 +2,12 @@ import datadog.trace.agent.test.AgentTestRunner
 import datadog.trace.instrumentation.api.Tags
 import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
-import java.util.concurrent.locks.Lock
-import java.util.concurrent.locks.ReentrantLock
-
 class SpringSchedulingTest extends AgentTestRunner {
-
-  Lock lock = new ReentrantLock()
-
-  def "schedule interval test"() {
-    setup:
-    def context = new AnnotationConfigApplicationContext(IntervalTaskConfig)
-    def task = context.getBean(IntervalTask)
-    
-    lock.lock()
-    TEST_WRITER.clear()
-
-    task.blockUntilExecute()
-
-    expect:
-    assert task != null
-    assertTraces(1) {
-      trace(0, 1) {
-        span(0) {
-          resourceName "IntervalTask.run"
-          operationName "scheduled.call"
-          parent()
-          errored false
-          tags {
-            "$Tags.COMPONENT" "spring-scheduling"
-            defaultTags()
-          }
-        }
-      }
-    }
-    lock.unlock()
-
-
-  }
 
   def "schedule trigger test according to cron expression"() {
     setup:
     def context = new AnnotationConfigApplicationContext(TriggerTaskConfig)
     def task = context.getBean(TriggerTask)
-
-    lock.lock()
-    TEST_WRITER.clear()
 
     task.blockUntilExecute()
 
@@ -66,6 +27,31 @@ class SpringSchedulingTest extends AgentTestRunner {
         }
       }
     }
-    lock.unlock()
+  }
+  
+  def "schedule interval test"() {
+    setup:
+    def context = new AnnotationConfigApplicationContext(IntervalTaskConfig)
+    def task = context.getBean(IntervalTask)
+
+    task.blockUntilExecute()
+
+    expect:
+    assert task != null
+    assertTraces(1) {
+      trace(0, 1) {
+        span(0) {
+          resourceName "IntervalTask.run"
+          operationName "scheduled.call"
+          parent()
+          errored false
+          tags {
+            "$Tags.COMPONENT" "spring-scheduling"
+            defaultTags()
+          }
+        }
+      }
+    }
+
   }
 }
